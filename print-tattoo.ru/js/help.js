703fbb06675bde423913436b408fd571fa4e047a
Traniks <andert2017@gmail.com>
2023-12-28 15:02:01 +0300

    fix: buttons and add bottom block

--- print-tattoo.ru/js/help.js
+++ print-tattoo.ru/js/help.js
@@ -1,40 +1,47 @@
-const parents = document.querySelectorAll('.questions__point')
-const buttons = document.querySelectorAll('.questions__point-title')
-const contents = document.querySelectorAll('.questions__point-description')
+const parents = document.querySelectorAll('.questions__point');
+const buttons = document.querySelectorAll('.questions__point-title');
+const contents = document.querySelectorAll('.questions__point-description');
+const pluss = document.querySelectorAll('.plus')
 
 buttons.forEach(btn => {
 	btn.addEventListener('click', e => {
-        if (e.target && e.target.classList.contains('questions__point-title_active') && e.target.classList.contains('questions__point-title')) {
-            const parent = btn.parentNode
-            const content = parent.querySelector('.questions__point-description_active')
+        if (e.target && e.target.classList.contains('questions__point-title') || e.target.classList.contains('plus')) {
 
-            parent.classList.remove('questions__point_active')
-            btn.classList.remove('questions__point-title_active')
-            content.classList.remove('questions__point-description_active')
-        } else if(e.target && e.target.classList.contains('questions__point-title')) {
-            if (e.target && e.target.classList.contains('questions__point-title')) {
-                const parent = btn.parentNode
-                const content = parent.querySelector('.questions__point-description')
-    
+            // console.log(e.target.classList.contains('plus'))
+			// console.log(btn.parentNode)
+
+            if (e.target.classList.contains('questions__point-title_active') || e.target.classList.contains('plus_active')) {
                 hide()
-                parent.classList.add('questions__point_active')
-                btn.classList.add('questions__point-title_active')
-                content.classList.add('questions__point-description_active')
-            }
+            } else {
+				const parent = btn.parentNode
+				const content = parent.querySelector('.questions__point-description')
+				const plus = parent.querySelector('.plus')
+
+                hide()
+				parent.classList.add('questions__point_active')
+				btn.classList.add('questions__point-title_active')
+                plus.classList.add('plus_active')
+				content.classList.add('questions__point-description_active')
+			}
+
         }
 	})
 })
 
 function hide() {
 	parents.forEach(item => {
-		item.classList.remove('questions__point_active')
+		item.classList.remove('questions__point_active');
 	})
 
 	buttons.forEach(item => {
-		item.classList.remove('questions__point-title_active')
+		item.classList.remove('questions__point-title_active');
+	})
+
+    pluss.forEach(item => {
+		item.classList.remove('plus_active')
 	})
 
 	contents.forEach(item => {
-		item.classList.remove('questions__point-description_active')
+		item.classList.remove('questions__point-description_active');
 	})
 }
